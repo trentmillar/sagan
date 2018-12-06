2fd3615c3f89d6f74d71c364d4bf2feccf023952
trentmillar <trent.millar@gmail.com>
2018-12-06 11:38:29 -0700

    sitting on this project for awhile, checking in current state

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Sagan
+
+### Schemas
+
+##### creating
+- create a Schema then use `add` and pass the object
+- pass the object and optional option when instantiating the `Schema`
+- create `Model` passing the collection name, then the object to create the `Schema`
--- test/schema.test.js
+++ test/schema.test.js
@@ -1,13 +1,18 @@
 let shared = require('./_shared')(),
-    sagan = require('../lib'),
+    sagan = require('../lib')('sql'),
     Schema = sagan.Schema;
-
-
 
 describe('schema', function() {
 
+  let opts = {
+    Database: 'sagan_test',
+    connectionPolicy : {
+      RequestTimeout: 5000
+    }
+  };
+
   before(function(done) {
-    sagan.connect(process.env.ACCOUNT_ENDPOINT, process.env.AUTH_KEY);
+    //sagan.connect(process.env.ACCOUNT_ENDPOINT, process.env.AUTH_KEY, opt);
     return done();
   });
 
@@ -18,12 +23,53 @@ describe('schema', function() {
       title: {
         type: String
       },
+      slug: {
+        type: String
+      },
       createdAt: Date,
       content: String
     });
 
+    review.set('collection', 'revs');
+    review.set('partition', 'title');
+
+    /**
+     * Plugins
+     */
+
+    function slugGenerator(options) {
+      options = options || {};
+      var key = options.key || 'title';
+
+      return function slugGenerator(schema) {
+        schema.path(key).set(function(v) {
+          this.slug = v.toLowerCase().replace(/[^a-z0-9]/g, '').replace(/-+/g, '');
+          return v;
+        });
+      };
+    }
+
+    review.plugin(slugGenerator());
+
     sagan.model('Review', review);
 
-    return done();
+    sagan.model('User', {
+      name: String
+    });
+
+    sagan.connect(process.env.ACCOUNT_ENDPOINT, process.env.AUTH_KEY, opts, function(err, client) {
+
+      let model = new (sagan.model('Review'))();
+      model.title = "My post";
+      model.content = "Content goes here!";
+
+     model.save(function(err, result) {
+      console.log(err);
+
+      });
+
+    //return done();
+    });
+
   });
 });
